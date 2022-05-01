dd5d91db07154d05e926d5829d39e756e84f8fdd
Federico <wuthrich09@gmail.com>
2022-05-01 00:30:13 -0300

    Js carrito de compras Soragna

--- js/script.js
+++ js/script.js
@@ -1,57 +1,124 @@
-/*Declaracion de arrays */
-let pizzas = ["Margherita", "4 stagioni", "Esuberante", "La regina"];
-let pastas = ["Quattro Formaggi", "Renato", "Frutti di mare", "Parisienne"];
-let vinos = ["Espumantes", "Rosso", "Bianco", "Blends"];
-let carta = {
-    pizzas: pizzas,
-    pastas: pastas,
-    vinos: vinos,
-}
-/*array del carrito a pushear */
-let carrito = []
 /*Saludo al usuario */
+const divSaludo = document.querySelector("#SaludoUsuario");
+const containerJs = document.querySelector("#CarritoDeCompras")
 const saludar = () => {
-    let nombre;
-
-    do {
-        nombre = prompt("Indicanos tu nombre :");
-
-    } while (nombre === "" || !isNaN(nombre));
-    alert(`¡Ciao ${nombre}!, bienvenido a Soragna`);
-};
-saludar()
-/*Funcion principal */
-const productos = () => {
-    let opcionesProductos = parseInt(prompt(`Elija una de las siguientes opciones del menu:\n 1)Pizzas\n 2)Pastas\n 3)Vinos`));
-    let nombre;
-    let opcProductosAux;
-    switch (opcionesProductos) {
-        case 1:
-            nombre = "Pizzas"
-            opcProductosAux = carta["pizzas"]
-            break
-        case 2:
-            nombre = "Pastas"
-            opcProductosAux = carta["pastas"]
-            break
-        case 3:
-            nombre = "Vinos"
-            opcProductosAux = carta["vinos"]
-            break
-    };
- subProducto(nombre, opcProductosAux)
- subProducto(nombre, opcProductosAux)
- subProducto(nombre, opcProductosAux)
- alert("USTED HA ELEGIDO LO SIGUIENTE:\n" + carrito)
-};
-
-const subProducto = (nombre, array) => {
-    let mensaje = ""
-    for (let i = 0; i < array.length; i++) {
-        mensaje += `\n ${i+1}) ${array[i]}` 
+    alert("¡Bienvenido a Soragna Pizza e Pasta!");
+    let nombre = prompt("Ingrese su nombre: ");
+    while(nombre === "" || !isNaN(parseInt(nombre))){
+        nombre = prompt("Ingrese solo valores alfabéticos por favor : ")
     }
-    let opcionesProductos = parseInt(prompt(`usted eligió ${nombre} las siguientes opciones son: ${mensaje}`));
-    carrito.push(array[opcionesProductos - 1]) 
+    divSaludo.innerHTML = `<h2>!Ciao, ${nombre.toUpperCase()}!</h2>`
+};
+
+saludar();
+
+
+//Constructor de carta 
+class Productos {
+    constructor(id, tipo, nombre, precio) {
+        this.id = id;
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+//Pizzas
+const Margherita = new Productos(1,"Pizza","Margherita",950);
+const cuatroStagioni = new Productos(2,"Pizza","4 Stagioni",1000);
+const Esuberante = new Productos(3,"Pizza","Esuberante",1200);
+const LaRegina  = new Productos(4,"Pizza","La Regina",1200);
+//Pastas
+const QuattroFormaggi = new Productos(5,"Pasta","Quattro Formaggi",520);
+const Renato = new Productos(6,"Pasta","Renato",750);
+const FruttiDiMare = new Productos(7,"Pasta","Frutti di Mare",980);
+const Parisienne = new Productos(8,"Pasta","Parissienne",650);
+//Vinos
+const Espumantes = new Productos(9,"Vino","Espumante",1500);
+const Rosso = new Productos(10,"Vino","Rosso",2000);
+const Bianco = new Productos(11,"Vino","Bianco",950);
+const Blends = new Productos(12,"Vino","Blends",1100);
+
+//Arrays a utilizar
+let Carta = [Margherita,cuatroStagioni,Esuberante,LaRegina,
+    QuattroFormaggi,Renato,FruttiDiMare,Parisienne,Espumantes,Rosso,Bianco,Blends]
+const carrito = []
+
+const consultarCarta = () =>{
+    let texto = "";
+    for(const a of Carta){
+        texto += `${a.id}) ${a.tipo} ${a.nombre} ${a.precio}\n`
+    }
+    let prod = parseInt(prompt(`Que producto desea llevar?:\n${texto} `))
+
+    if(prod >12 || prod < 1 || isNaN(prod)){
+        prod = parseInt(prompt(`Por favor, ingrese una valor admitido: \n${texto}`));
+    }
+    return prod;
+};
+
+let productosSeleccionados;
+
+// funcion donde elegís lo que queres comprar
+
+const llevarProducto = () =>{
+    let buscarProducto = Carta.find(
+        (element) => element.id === productosSeleccionados 
+    );
+    
+
+    
+    let existencias = carrito.some((element) => element.id === productosSeleccionados)
+    console.log(existencias);
+
+    if(existencias){
+        buscarProducto.cantidad++
+    }else{
+        buscarProducto.cantidad = 1
+        carrito.push(buscarProducto);
+    };
+
+
+
+    const continuar = confirm("Desea seguir comprando?");
+    
+    if(continuar){
+        productosSeleccionados = consultarCarta();
+        llevarProducto()
+    }
 }
 
-productos()
+//funcion donde te muestra lo que elegiste y subtotal
+
+const mostrarProductos = () =>{
+    const divPedidos = document.createElement("div");
+    divPedidos.className = "pedido fw-bold text-white border border-3 border-success mt-3";
+    containerJs.appendChild(divPedidos);
+
+    carrito.forEach((element) => {
+        divPedidos.innerHTML += `<div class="ordenes mx-auto bg-info">
+        <h3>${element.nombre.toUpperCase()}</h3>
+        <h4> Precio unitario: ${element.precio} </h4>
+        <h4> Cantidad:  ${element.cantidad} </h4>
+        <p> Subtotal: ${element.precio * element.cantidad} </p>
+
+        </div>`
+    });
+};
+
+//funcion donde te muestra cuanto pagas por todos los productos elegidos
+
+const calcularTotal = () =>{
+    let montofinal = document.createElement("div")
+    montofinal.className = "ordenes mx-auto bg-success text-white"
+
+    let total = carrito.reduce((acc, ite)=>acc + ite.precio * ite.cantidad ,0)
+
+    montofinal.innerHTML = `<h2>El total es : $${total}</h2>`
+
+    containerJs.appendChild(montofinal);
+}
+
+ productosSeleccionados = consultarCarta();
+ llevarProducto();
+mostrarProductos();
+calcularTotal();
