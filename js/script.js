9269d44ec6f5579979c64baf45a247110abf0202
Federico <wuthrich09@gmail.com>
2022-05-17 20:37:01 -0300

    FIX: JS

--- js/script.js
+++ js/script.js
@@ -1,175 +1,142 @@
-/*Saludo al usuario */
-const saludar = () => {
-    let titleSaludo = document.querySelector("#titleSaludo");
-    let inputNombre = document.querySelector('#inputNombre')
-    let formSaludo = document.querySelector('#formSaludo')
-    
-    formSaludo.addEventListener('submit', (e) =>{
-        e.preventDefault();
-        titleSaludo.innerHTML = `<h1>¡Benvenuto! ${inputNombre.value.toUpperCase()}, esto es : </h1>`
-    });
-};
-saludar()
+
 //mi base de datos con mis productos
 const Carta = [
     {
+        id: 0,
+        tipo: "Pizza",
+        nombre: "Salsiccia",
+        precio: 950,
+        cantidad: 1,
+        imagen: "../assets/images/pizzaCard1.jpg"
+    },
+    {
         id: 1,
         tipo: "Pizza",
-        nombre:"Salsiccia",
-        precio: 950,
-
+        nombre: "Margherita",
+        precio: 1000,
+        cantidad: 1
     },
     {
         id: 2,
         tipo: "Pizza",
-        nombre:"Margherita",
-        precio: 1000,
-
+        nombre: "Rucula",
+        precio: 1200,
+        cantidad: 1
     },
     {
         id: 3,
         tipo: "Pizza",
-        nombre:"Rucula",
+        nombre: "Aglio e Cepolla",
         precio: 1200,
-
+        cantidad: 1
     },
     {
         id: 4,
-        tipo: "Pizza",
-        nombre:"Aglio e Cepolla",
-        precio: 1200,
-
+        tipo: "Pasta",
+        nombre: "Spaghetti alla Carbonara",
+        precio: 520,
+        cantidad: 1
     },
     {
         id: 5,
         tipo: "Pasta",
-        nombre:"Spaghetti alla Carbonara",
-        precio: 520,
-
+        nombre: "Penne Rigatti Mediterráneo",
+        precio: 750,
+        cantidad: 1
     },
     {
         id: 6,
         tipo: "Pasta",
-        nombre:"Penne Rigatti Mediterráneo",
-        precio: 750,
-
+        nombre: "Frutti di Mare",
+        precio: 980,
+        cantidad: 1
     },
     {
         id: 7,
         tipo: "Pasta",
-        nombre:"Frutti di Mare",
-        precio: 980,
-
+        nombre: "Pappardelle alla Bolognese",
+        precio: 650,
+        cantidad: 1
     },
     {
         id: 8,
-        tipo: "Pasta",
-        nombre:"Pappardelle alla Bolognese",
-        precio: 650,
-
+        tipo: "Vino",
+        nombre: "Chateaneuf du Pape",
+        precio: 1500,
+        cantidad: 1
     },
     {
         id: 9,
         tipo: "Vino",
-        nombre:"Chateaneuf du Pape",
-        precio: 1500,
-
+        nombre: "Campo Viejo Rioja DOM",
+        precio: 950,
+        cantidad: 1
     },
     {
         id: 10,
         tipo: "Vino",
-        nombre:"Campo Viejo Rioja DOM",
-        precio: 950,
-
+        nombre: "Burra Brook vino blanco",
+        precio: 1100,
+        cantidad: 1
     },
     {
         id: 11,
         tipo: "Vino",
-        nombre:"Burra Brook vino blanco",
-        precio: 1100,
-
-    },
-    {
-        id: 12,
-        tipo: "Vino",
-        nombre:"Madame Cliquot",
+        nombre: "Madame Cliquot",
         precio: 15000,
-
-    }
-]
-
-let carrito = JSON.parse(localStorage.getItem('carrito')) ?? []
-let bodyCarrito = document.querySelector("#canvaCarritoBody")
-
-let seleccionarProducto = () =>{
-    let divCards = document.getElementById('divCards')
-    divCards.addEventListener('click', (e) =>{
-        añadirCarrito(e);
-    })
-}
-
-let añadirCarrito = (e) =>{
-    if(e.target.querySelector('#btnCarrito')){
-        setearCarrito(e.target.carrito)
-    }
-}
-//declaro como se cargan los productos a mi carrito
-setearCarrito = (Carta) => {
-    let producto = {
-        id: Carta.querySelector('#btnCarrito').id,
-        nombre: Carta.nombre,
-        precio: Carta.precio,
         cantidad: 1
-    }
-    if(carrito.hasOwnProperty(producto.id)){
-        producto.cantidad = carrito[producto.id]++
-    }
+    },
+];
 
-    carrito[producto.id] = {...producto}
+let carrito = JSON.parse(localStorage.getItem("carrito")) ?? [];
+let bodyCarrito = document.querySelector("#canvaCarritoBody");
+let totalCompra = document.querySelector('#totalCompra')
 
-    mostrarCarrito();
-}
-//funcion para imprimir mis productos en el off canva
-let mostrarCarrito = () =>{
-    bodyCarrito.innerHTML += `
-    <div class=" mx-auto bg-info">
-    <h3>${Carta.nombre}</h3>
-    <h4> Precio unitario: ${Carta.precio} </h4>
-    <h4> Cantidad:  ${Carta.cantidad} </h4>
-    </div>
-    `
-    localStorage.setItem("carrito", JSON.stringify(carrito))
-}
-//funcion de los botones de agregar producto y limpiar el carrito
-let botonCarrito = (e) =>{
-    //agregar
-    if(e.target.e.target.querySelector('#btnCarrito')){
-        const product = carrito[e.target.id]
-        product.cantidad++;
-        carrito[e.target.id] = {...product}
-        mostrarCarrito()
-    }
-    //eliminar
-    if(e.target.e.target.querySelector('#btnVaciarCarrito')){
-        const product = carrito[e.target.id]
-        product.cantidad--;
-        if(product === 0){
-            delete carrito[e.target.id]
+
+let añadirCarrito = (e) => {
+    if (carrito.find(producto => producto.id == e.target.id)) {
+        for (let index = 0; index < carrito.length; index++) {
+            const producto = carrito[index];
+            if (producto.id == e.target.id) {
+                producto.cantidad++
+            }
         }
-        mostrarCarrito()
-    }
-    
-}
-//funcion del total de la compra
-let mostrarTotal = () =>{
-    
-    let total = carrito.reduce((acc, ite)=>acc + ite.precio * ite.cantidad ,0)
-    bodyCarrito.innerHTML = `<h6 class="bg-danger">El total es : $${total}</h6>`
-    ;
+          
+      
+    } else
+        carrito.push(Carta[e.target.id])
+    console.log(carrito)
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+
+    subtotal()
 }
 
-//LocalStorage
-if (localStorage.getItem("carrito")) {
-    carrito = JSON.parse(localStorage.getItem("carrito"));
-    mostrarCarrito();
-  }
+let subtotal = () =>{
+    bodyCarrito.innerHTML = ``
+    carrito.forEach(Carta => {
+        bodyCarrito.innerHTML += `
+        <div class=" mx-auto bg-info">
+        <img src=${Carta.imagen} class="img-fluid" />
+        <h3>${Carta.nombre}</h3>
+        <h4> Precio unitario: ${Carta.precio} </h4>
+        <h4> Cantidad:  ${Carta.cantidad} </h4>
+        </div>
+        `
+    });
+    mostrarTotal()
+
+}
+
+let mostrarTotal = () => {
+    console.log("hola")
+    let total = carrito.reduce((acc, ite) => acc + ite.precio * ite.cantidad, 0);
+    totalCompra.innerHTML = `<h6 class="bg-danger">El total es : $ ${total} </h6>`;
+  };
+
+let vaciarCarrito = () => {
+    carrito = []
+    bodyCarrito.innerHTML = ''
+    totalCompra.innerHTML = ''
+
+    localStorage.setItem("carrito",JSON.stringify(carrito))
+}
